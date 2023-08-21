2dda1bb5dcb94b3ce14fa82dc0691423bdd4e422
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-08-21 14:08:24 -0700

    feat: add demo instructions

---
+++ timegpt-launch-sf/demo.py
@@ -0,0 +1,10 @@
+# Typically, this would involve building a complex pipeline, fitting different models, and tuning hyperparameters. 
+# With TimeGPT, I'll demonstrate how this can be achieved in under 10 lines of code during a live demo.
+
+# Import necessary libraries
+
+# Load and visualize historical data
+
+# Authenticate with TimeGPT
+
+# Generate and visualize forecats
