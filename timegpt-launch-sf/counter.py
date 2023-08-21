79c4221ea735c8f064eb08612957248d32ad8789
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-08-21 16:09:48 -0700

    feat: add ascii rep

--- timegpt-launch-sf/counter.py
+++ timegpt-launch-sf/counter.py
@@ -2,6 +2,83 @@ import time
 from rich.console import Console
 from rich.align import Align
 
+def number_representation(n):
+    numbers = {
+        '0': [
+            ' 0000 ',
+            '00  00',
+            '00  00',
+            '00  00',
+            ' 0000 '
+        ],
+        '1': [
+            '1111  ',
+            '  11  ',
+            '  11  ',
+            '  11  ',
+            '111111'
+        ],
+        '2': [
+            ' 2222 ',
+            '22  22',
+            '   22 ',
+            '  22  ',
+            '222222'
+        ],
+        '3': [
+            ' 3333 ',
+            '33  33',
+            '   333',
+            '33  33',
+            ' 3333 '
+        ],
+        '4': [
+            '44  44',
+            '44  44',
+            '444444',
+            '    44',
+            '    44'
+        ],
+        '5': [
+            '555555',
+            '55    ',
+            '55555 ',
+            '    55',
+            '55555 '
+        ],
+        '6': [
+            ' 6666 ',
+            '66    ',
+            '66666 ',
+            '66  66',
+            ' 6666 '
+        ],
+        '7': [
+            '777777',
+            '   77 ',
+            '  77  ',
+            ' 77   ',
+            '77    '
+        ],
+        '8': [
+            ' 8888 ',
+            '88  88',
+            ' 8888 ',
+            '88  88',
+            ' 8888 '
+        ],
+        '9': [
+            ' 9999 ',
+            '99  99',
+            ' 99999',
+            '    99',
+            ' 9999 '
+        ]
+    }
+    
+    # Return representation for a given number
+    return '\n'.join(numbers[str(n)])
+
 console = Console()
 console.clear()
 
@@ -27,7 +104,7 @@ def centered_counter(duration=10):
     time.sleep(2)
     for i in range(duration-1, -1, -1):
         console.clear()
-        center_print(f'[bold green]{i}[/bold green]')
+        center_print(f'[bold green]{number_representation(i)}[/bold green]')
         time.sleep(1)
     console.clear()
     center_print("[bold blue]Happy Forecasting! 😃🎉[/bold blue]", 3)
