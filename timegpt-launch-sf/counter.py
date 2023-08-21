40a4180c129b5709b270ca565182f40ff872d799
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-08-21 16:05:14 -0700

    feat: style counter

--- timegpt-launch-sf/counter.py
+++ timegpt-launch-sf/counter.py
@@ -3,9 +3,17 @@ from rich.console import Console
 from rich.align import Align
 
 console = Console()
+console.clear()
+
+def vertical_padding(lines=1):
+    """Return vertical padding to center text vertically."""
+    height = console.size.height
+    padding_lines = (height - lines) // 2
+    return '\n' * padding_lines
 
 def center_print(message: str, duration: int = None):
     """Utility function to print centered text."""
+    console.print(vertical_padding())
     aligned_message = Align.center(message)
     console.print(aligned_message)
     if duration:
@@ -14,8 +22,10 @@ def center_print(message: str, duration: int = None):
 def centered_counter(duration=10):
     center_print("[bold yellow]Are you ready for TimeGPT?[/bold yellow]")
     time.sleep(2)
-    console.print("\n")
-    for i in range(duration, -1, -1):
+    console.clear()
+    center_print("[bold violet] Let's count together from 9 to 0 (because of Python xD)[/bold violet]")
+    time.sleep(2)
+    for i in range(duration-1, -1, -1):
         console.clear()
         center_print(f'[bold green]{i}[/bold green]')
         time.sleep(1)
