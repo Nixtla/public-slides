7826807a2e0a073a38909068c4dff05d321b6d8d
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-08-21 13:44:40 -0700

    fix: simplify text

--- timegpt-launch-sf/setup.sh
+++ timegpt-launch-sf/setup.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/nixtla/transfer-learning-time-series
-cp transfer-learning-time-series/datasets/electricity-short.csv ./electricity-markets.csv
+cp transfer-learning-time-series/datasets/electricity-short.csv ./electricity.csv
 pip install -r requirements.txt && python -m src.filter_dataset && tmux
