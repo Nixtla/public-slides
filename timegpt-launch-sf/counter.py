7f3ccc9e1f4295478e9e65f7151504a8a4a1eeba
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-08-21 14:03:56 -0700

    feat: add counter script

---
+++ timegpt-launch-sf/counter.py
@@ -0,0 +1,27 @@
+import time
+from rich.console import Console
+from rich.align import Align
+
+console = Console()
+
+def center_print(message: str, duration: int = None):
+    """Utility function to print centered text."""
+    aligned_message = Align.center(message)
+    console.print(aligned_message)
+    if duration:
+        time.sleep(duration)
+
+def centered_counter(duration=10):
+    center_print("[bold yellow]Are you ready for TimeGPT?[/bold yellow]")
+    time.sleep(2)
+    console.print("\n")
+    for i in range(duration, -1, -1):
+        console.clear()
+        center_print(f'[bold green]{i}[/bold green]')
+        time.sleep(1)
+    console.clear()
+    center_print("[bold blue]Happy Forecasting! 😃🎉[/bold blue]", 3)
+
+if __name__ == "__main__":
+    centered_counter()
+
