28a837a1c46de87653a60014480b6abd2237fba2
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-09-07 05:46:09 +0200

    fix: rm trans talk

--- transformers-transexuals-platanus/setup.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/nixtla/transfer-learning-time-series
-cp transfer-learning-time-series/datasets/electricity-short.csv ./electricity.csv
-pip install -r requirements.txt && python -m src.filter_dataset && tmux
--- transformers-transexuals-platanus/src/filter_dataset.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('./electricity.csv')
-df = df.query('unique_id != "NP"')
-df.to_csv('./electricity.csv', index=False)
