4585baf43f72573acb24422fe3ef444a27b174fc
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-09-07 07:21:16 +0200

    feat: add demo skeleton

---
+++ timegpt-conformal-hierarchical-prato/demo.py
@@ -0,0 +1,13 @@
+# Probabilistic conformal prediction for hierarchical time series with TimeGPT
+
+# Import necessary libraries and authenticate
+
+# Load historical data
+
+# Predict the future 
+
+# Reconcile forecasts
+
+# Add prediction intervals
+
+# Visualize
