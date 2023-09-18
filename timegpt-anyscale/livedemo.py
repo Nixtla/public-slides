e163531bbc5383a6c323354298bd562dc6c3bd3d
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-09-18 14:52:22 -0700

    feat: add livedemo

---
+++ timegpt-anyscale/livedemo.py
@@ -0,0 +1,14 @@
+# Import necessary libraries and authenticate
+
+# Connect to ray cluster
+
+# Authorize timegpt
+
+# Load historical data distributedly (using Ray)
+
+# Predict the future distributedly (using Ray) 
+
+# Visualize
+
+
+
